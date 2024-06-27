09ee5331388e80bbbee5d9c1a7edd839e8edc855
nemofalcon <nemo.falcon96@gmail.com>
2024-06-27 13:54:10 +0500

    last

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/api/data', (req, res) => {
-    const data = {
-        message: 'Hello, this is your GET API endpoint!'
-    };
-    res.json(data); 
-    
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+const express = require("express");const app = express();// Respond with "hello world" for requests that hit our root "/"
+app.get("/", function (req, res) {
+ return res.send("Hello World");
+});// listen to port 7000 by default
+app.listen(process.env.PORT || 7000, () => {
+  console.log("Server is running");
+});module.exports = app;
