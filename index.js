d76a9f9695faa6db409317e2d94263c23b280915
nemofalcon <nemo.falcon96@gmail.com>
2024-06-27 11:26:38 +0500

    index

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/api/data', (req, res) => {
+    const data = {
+        message: 'Hello, this is your GET API endpoint!'
+    };
+    res.json(data); 
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
