c263ffd4fc4668c3f3e8521805abe7799d1f93df
nemofalcon <nemo.falcon96@gmail.com>
2024-06-28 09:10:44 +0500

    tests

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 app.get("/", function (req, res) {
  return res.send("Hello World");
-});// listen to port 7000 by default
+});
+// listen to port 7000 by default
 
 app.listen(process.env.PORT || 7000, () => {
   console.log("Server is running");
